389ce0536afc727b37e28a9aa876fb8e33821a8c
Igor Goryunov <kongostream@gmail.com>
2018-10-07 16:44:38 +0300

    add getContractMethods

--- executor.thrift
+++ executor.thrift
@@ -2,8 +2,7 @@ include "variant.thrift"
 namespace java com.credits.thrift.generated
 namespace cpp executor
 
-struct APIResponse
-{
+struct APIResponse{
     1: i8 code
     2: string message
 	3: binary contractState
@@ -11,7 +10,19 @@ struct APIResponse
 	5: optional map<string, variant.Variant> contractVariables
 }
 
-service ContractExecutor
-{
+struct MethodDescription {
+    1: string name
+    2: list<string> argTypes
+    3: string returnType
+}
+
+struct GetContractMethodsResult {
+    1: i8 code
+    2: string message
+    3: list<MethodDescription> methods
+}
+
+service ContractExecutor {
 	APIResponse executeByteCode(1:binary address, 2:binary byteCode, 3: binary contractState, 4:string method, 5:list<variant.Variant> params)
+	GetContractMethodsResult getContractMethods(1: binary bytecode)
 }
