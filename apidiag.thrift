a1d191279799c5e2002620a08992492682b2ed5f
Alexander Avramenko <aav@credits.com>
2020-01-17 19:06:37 +0300

    new file

---
+++ apidiag.thrift
@@ -0,0 +1,132 @@
+include 'general.thrift'
+
+namespace csharp NodeApiDiag
+namespace netcore NodeApiDiag
+namespace java com.credits.client.node.diag.thrift.generated
+namespace cpp api_diag
+
+struct TransactionId
+{
+    // block
+    1: i64 sequence
+    // 0-based index in block
+    2: i16 index
+}
+
+// Unix timestamp in milliseconds
+typedef i64 Time
+
+typedef i64 TransactionInnerId
+
+struct AmountCommission
+{
+  1: required i16 bits
+  2: double value
+}
+
+struct SmartContractDeploy
+{
+  1: string sourceCode
+  2: list<general.ByteCodeObject> byteCodeObjects
+  3: string hashState
+  4: i32 tokenStandard
+}
+
+struct SmartContractInvocation
+{
+  1: string method
+  // Empty on deploy, method params stringified Java-side with conversion to string on execute
+  2: list<general.Variant> params //general.Variant+
+  // If true, do not emit any transactions to blockchain (execute smart contract and forget state change if any)
+  3: list<general.Address> usedContracts
+  // try call as getter
+  4: bool getter
+  5: i16 version = 1; // must be on this place! Position cannot be changed
+}
+
+union UserFielData
+{
+    1: i64 integer
+    // Binary data or text
+    2: string bytes
+    3: general.Amount amount
+    // Contract deployment
+    4: SmartContractDeploy deploy
+    // Contract invocation
+    5: SmartContractInvocation execute
+}
+
+struct UserField
+{
+    1: i8 id
+    2: UserFielData data
+}
+
+enum TransactionType
+{
+    // CS transfer
+    TT_Transfer,
+    // contarct deployment
+    TT_ContractDeploy,
+    // Contract execution
+    TT_ContractExecute,
+    // Contract replenish (indirect payable() invocation) 
+    TT_ContractReplenish,
+    // Contract new state
+    TT_ContractState,
+    // Token deployment
+    TT_TokenDeploy,
+    // Token transfer
+    TT_TokenTransfer,
+    // Stake delegation to node address
+    TT_Delegation,
+    // Cancel stake delegation
+    TT_RevokeDelegation,
+    // Put some transfer on hold until some codition to release or cancel
+    TT_Hold,
+    // Release previously hold sum to complete transfer
+    TT_Release,
+    // Revoke hold to cancel the transfer
+    TT_CancelHold,
+    // Transfer delayed until some moment
+    TT_DelayedTransfer,
+    // Service: update current boostrap node list with new one
+    TT_UpdateBootstrapList,
+    // Any other type
+    TT_Other
+}
+
+struct TransactionData
+{
+    // Inner transaction ID for protection against replay attack
+    1: TransactionInnerId id
+    // Sender or contract deployer/invoker.
+    // In general, the public key to test signature
+    2: general.Address source
+    // Receiver or contract address
+    3: general.Address target
+    // Transfer amount for payment transaction
+    4: general.Amount amount
+    // Fee limitation
+    5: AmountCommission max_fee
+    // Actual fee to withdraw from source
+    6: AmountCommission actual_fee
+    // Signature is formed against node's custom binary serialization format,
+    // see other docs for description
+    7: binary signature
+    8: optional SmartContractInvocation smartContract
+    9: Time timestamp
+    10: TransactionType type
+    // user fields if any
+    11: optional list<UserField> userFields
+}
+
+struct GetTransactionResponse
+{
+    1: general.APIResponse status
+    2: optional TransactionData transaction
+}
+
+service API_DIAG {
+	GetTransactionResponse GetTransaction(1:TransactionId id)
+}
